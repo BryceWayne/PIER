3565ca032f3200591942a66cb38020cbc47d5409
Batman <35088752+BryceWayne@users.noreply.github.com>
2020-01-08 15:48:52 -0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,10 +10,8 @@ import os
 import pandas as pd
 
 SIZE = 800
-cwd = os.getcwd()
-print(cwd)
 """ GET DATA """
-data2 = pd.read_csv(os.path.join(cwd, 'data/A15226.txt'), sep="\t")
+data2 = pd.read_csv('./data/A15226.txt', sep="\t")
 # print(data2.head)
 data2 = data2.iloc[1:].dropna()
 data2['Date'] = pd.to_datetime(data2['Date local DST']).dropna()
